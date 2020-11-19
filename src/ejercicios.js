3ca38e0bc230452613015f6b7e4d21642b0ef129
Eduardo Cuomo <reduardo7@gmail.com>
2020-11-19 18:51:55 -0300

    Resolución Try & Catch

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -1,12 +1,21 @@
 const db = new BaseDatos();
-db.connect();
+
+if (new Date().getTime() % 2) {
+  db.connect();
+}
 
 function leerDesdeDB() {
-  Utils.disableAll();
-  const id = prompt('ID a leer:');
-  const data = db.read(id);
-  Utils.enableAll();
-  return data;
+  try {
+    Utils.disableAll();
+    const id = prompt('ID a leer:');
+    const data = db.read(id);
+    return data;
+  } catch (err) {
+    alert(`Error!\n\n\t${err.message}`);
+    return null;
+  } finally {
+    Utils.enableAll();
+  }
 }
 
 function btnLeer() {
@@ -14,17 +23,22 @@ function btnLeer() {
   console.info('Resultado:', data);
 }
 
+function btnCargar() {
+  try {
+    Utils.disableAll();
+    const id = prompt('Nuevo ID:');
+    const dato = prompt('Nuevo Dato:');
+    db.save(id, dato);
+  } catch (err) {
+    alert(`Error al cargar!\n\n\t${err.message}`);
+  } finally {
+    Utils.enableAll();
+  }
+}
+
 // function btnConectar() {
 //   Utils.disableAll();
 //   db.connect();
-//   Utils.enableAll();
-// }
-
-// function btnCargar() {
-//   Utils.disableAll();
-//   const id = prompt('Nuevo ID:');
-//   const dato = prompt('Nuevo Dato:');
-//   db.save(id, dato);
 //   Utils.enableAll();
 // }
 
