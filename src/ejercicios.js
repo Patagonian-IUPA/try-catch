9d4807cde0963f0589c8db701e5369d5a1ab6ec9
Arcaytus <manu.martinezgalan@gmail.com>
2020-11-29 16:41:55 -0300

    Added "try, catch and finally" to the functions

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -1,35 +1,104 @@
 const db = new BaseDatos();
 db.connect();
 
+try {
 function leerDesdeDB() {
+  try {
   Utils.disableAll();
+  } catch (err) {
+    console.log ('Error. disableAll cannot be found in the selected file');
+  } finally {
+    console.log ('Finally');
+  }
   const id = prompt('ID a leer:');
   const data = db.read(id);
+  try {
   Utils.enableAll();
+  } catch (err) {
+    console.log ('Error. enableAll cannot be found in the selected file');
+  } finally {
+    console.log ('Finally');
+  }
   return data;
 }
+} catch (ex) {
+  console.error ('Outer', 'Error. This function does not exist');
+}
 
+
+try {
 function btnLeer() {
   const data = leerDesdeDB();
   console.info('Resultado:', data);
 }
+} catch (ex) {
+   console.error ('Outer', 'Error. This function does not exist');
+}
 
-// function btnConectar() {
-//   Utils.disableAll();
-//   db.connect();
-//   Utils.enableAll();
-// }
 
-// function btnCargar() {
-//   Utils.disableAll();
-//   const id = prompt('Nuevo ID:');
-//   const dato = prompt('Nuevo Dato:');
-//   db.save(id, dato);
-//   Utils.enableAll();
-// }
+try {
+function btnConectar() {
+  try {
+ Utils.disableAll();
+  } catch (err) {
+    console.log ('Error. disableAll cannot be found in the selected file');
+  } finally {
+    console.log ('Finally');
+  }
+ db.connect();
+ try {
+ Utils.enableAll();
+  } catch (err) {
+   console.log ('Error. enableAll cannot be found in the selected file');
+  } finally {
+    console.log ('Finally');
+  }
+}
+} catch (ex) {
+  console.error ('Outer', 'Error. This function does not exist');
+}
 
-// function btnDesconectar() {
-//   Utils.disableAll();
-//   db.close();
-//   Utils.enableAll();
-// }
+
+try {
+function btnCargar() {
+  try {
+  Utils.disableAll();
+  } catch (err) {
+    console.log ('Error. disableAll cannot be found in the selected file');
+  } finally {
+    console.log ('Finally');
+  }
+   const id = prompt('Nuevo ID:');
+   const dato = prompt('Nuevo Dato:');
+   db.save(id, dato);
+   try {
+   Utils.enableAll();
+  } catch (err) {
+    console.log ('Error. enableAll cannot be found in the selected file');
+  } finally {
+    console.log ('Finally');
+  }
+}
+} catch (ex) {
+  console.error ('Outer', 'Error. This function does not exist');
+}
+
+
+
+try {
+function btnDesconectar() {
+  try {
+   Utils.disableAll();
+  } catch (err){
+  console.log ('Error. disableAll cannot be found in the selected file');
+  }
+   db.close();
+  try {
+   Utils.enableAll();
+  } catch (err){
+    console.log ('Error. enableAll cannot be found in the selected file');
+  }
+}
+} catch (ex) {
+  console.error ('Outer', 'Error. This functions does not exist');
+}
