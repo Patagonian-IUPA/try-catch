b0bb51734afe0408a2fd26c63363d0ac06da4f13
Lausi Juan Pablo <juanpablo_lausi@hotmail.com.ar>
2020-12-01 17:26:20 -0300

    Entrega-Uno

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -2,11 +2,18 @@ const db = new BaseDatos();
 db.connect();
 
 function leerDesdeDB() {
+ try {
   Utils.disableAll();
   const id = prompt('ID a leer:');
   const data = db.read(id);
-  Utils.enableAll();
   return data;
+ } catch (error) {
+  alert(`Error!\n\n ${error.message}`)
+   return null;
+ } finally{
+  Utils.enableAll();
+ }
+  
 }
 
 function btnLeer() {
@@ -14,22 +21,45 @@ function btnLeer() {
   console.info('Resultado:', data);
 }
 
-// function btnConectar() {
-//   Utils.disableAll();
-//   db.connect();
-//   Utils.enableAll();
-// }
+function btnCargar() {
+  try {
+    Utils.disableAll();
+    const id = prompt('Nuevo ID:');
+    const dato = prompt('Nuevo Dato:');
+    db.save(id, dato);
+  } catch (error) {
+    alert(`Error al cargar!\n\n ${error.message}`)
+    return null;
+  } finally{
+    Utils.enableAll();
+  }
+  
+}
 
-// function btnCargar() {
-//   Utils.disableAll();
-//   const id = prompt('Nuevo ID:');
-//   const dato = prompt('Nuevo Dato:');
-//   db.save(id, dato);
-//   Utils.enableAll();
-// }
+function btnConectar() {
+  try {
+    Utils.disableAll();
+    db.connect();
+  } catch (error) {
+    alert(`Error al conectar!\n\n ${error.message}`)
+    return null;
+  } finally{
+    Utils.enableAll();
+  }
+  
+}
 
-// function btnDesconectar() {
-//   Utils.disableAll();
-//   db.close();
-//   Utils.enableAll();
-// }
+
+
+function btnDesconectar() {
+  try {
+    Utils.disableAll();
+    db.close();
+  } catch (error) {
+    alert(`Error al desconectar!\n\n ${error.message}`)
+    return null;
+  } finally{
+    Utils.enableAll();
+  }
+  
+}
