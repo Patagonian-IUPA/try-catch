408d528a1ea82dd2d478188cc42a7ccc5ca30adb
Franck_Tscherig <fjtsystemsg@gmail.com>
2020-12-01 00:32:16 -0300

    Alumno: Tscherig Francisco - Resolución TP nº3

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
 # Patagonian | IUPA > Programación Web 1
 
 ## Manejo de Errores
-
+---
+title: "Trabajo Practico Nº:3 “Para este trabajo se utilizan los conceptos vistos en clase, y el repositorio público que se comento en la clase nº 6."
+author: "**Ing. Francisco Tscherig**"
+date: "24/11/2020"
+---
+[![N|Solid](https://i.ibb.co/Lx0x9VH/iupa.png)](https://iupa.edu.ar/campus/login/index.php)
 ### Ejercicios
 
 Modificar el código en [`/src/ejercicios.js`](/src/ejercicios.js)
 para que se controlen los errores en caso de ocurrir alguno.
 
-1. El usuario debe poder continuar en caso de encontrar
+| 1 | El usuario debe poder continuar en caso de encontrar
    algún error.
-2. El usuario debe visualizar un mensaje claro en caso de
+| 2 | El usuario debe visualizar un mensaje claro en caso de
    existir algún error.
-3. La página no debe perder funcionalidad.
+| 3 | La página no debe perder funcionalidad.
 
 ### Documentación de referencia
 
--- src/ejercicios.js
+++ src/ejercicios.js
@@ -1,35 +1,72 @@
 const db = new BaseDatos();
-db.connect();
+// para prueba de no conectado
+if (new Date().getTime()%2){
+db.connect();}
+
 
 function leerDesdeDB() {
+  try{
   Utils.disableAll();
   const id = prompt('ID a leer:');
   const data = db.read(id);
   Utils.enableAll();
-  return data;
+  return data;}
+  catch(err){
+    // error
+    //alert('Error- Vuelva a  ingresar el ID a leer');
+    alert(`Error- Vuelva a  ingresar el ID a leer!\n\n\t${err.message}`);
+    console.warn('Error- Vuelva a  ingresar el ID a leer',err);
+    //se vuelve a habilitar el boton
+   // Utils.enableAll();
+    return null;
+  }finally{Utils.enableAll();}
 }
 
 function btnLeer() {
+  try{
   const data = leerDesdeDB();
-  console.info('Resultado:', data);
+  console.info('Resultado:', data);}
+  catch(err){  alert('Error al leer desde DB');
+  alert(`Error al leer desde DB\n\n\t${err.message}`);
+
+  console.warn('Error al leer desde DB',err);}}
+
+function btnConectar() {
+  try{
+  Utils.disableAll();
+     db.connect();
+    //Utils.enableAll();
+  }
+  catch(err){  alert('Error al conectar');
+  alert(`Error al conectar\n\n\t${err.message}`);
+  
+  console.warn('Error al conectar',err);}
+finally{Utils.enableAll();}}
+
+
+ function btnCargar() {
+
+ try{
+  Uils.disableAll();
+   const id = prompt('Nuevo ID:');
+   const dato = prompt('Nuevo Dato:');
+   db.save(id, dato);  
+ }
+ catch(err){
+   alert(`Error al cargar!\n\n\t${err.message}`)
+   console.warn('Error al cargar',err);
+ } finally
+ {Utils.enableAll();}
+ }
+
+ function btnDesconectar() {
+  try{
+   Utils.disableAll();
+   db.close();
+ }
+catch(err){
+  alert(`Error al Desconectar\n\n\t${err.message}`)
+  console.warn('Error al Desconectar',err);
+} finally
+{Utils.enableAll();}
 }
-
-// function btnConectar() {
-//   Utils.disableAll();
-//   db.connect();
-//   Utils.enableAll();
-// }
-
-// function btnCargar() {
-//   Utils.disableAll();
-//   const id = prompt('Nuevo ID:');
-//   const dato = prompt('Nuevo Dato:');
-//   db.save(id, dato);
-//   Utils.enableAll();
-// }
-
-// function btnDesconectar() {
-//   Utils.disableAll();
-//   db.close();
-//   Utils.enableAll();
-// }
