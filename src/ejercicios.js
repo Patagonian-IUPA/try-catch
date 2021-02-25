d109d2883292fa9ea2f6c5645ec6a1a04d822bf5
Gabriel Raby <grabyc@gmail.com>
2021-02-25 19:06:10 -0300

    PW1_TP_ErrorHandling - Resuelto OK

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -3,33 +3,86 @@ db.connect();
 
 function leerDesdeDB() {
   Utils.disableAll();
-  const id = prompt('ID a leer:');
-  const data = db.read(id);
-  Utils.enableAll();
+  const id = prompt("ID a leer:");
+  let data = null;
+
+  try {
+    data = db.read(id);
+  } catch (err) {
+    let strTipoError;
+
+    if (err instanceof DBError) {
+      strTipoError = "Base de Datos";
+    } else {
+      strTipoError = "Sistema";
+    }
+
+    console.error(err);
+    throw new Error(`Error de ${strTipoError}: ${err.message}`);
+  } finally {
+    Utils.enableAll();
+  }
+
   return data;
 }
 
-function btnLeer() {
-  const data = leerDesdeDB();
-  console.info('Resultado:', data);
+function btnConectar() {
+  Utils.disableAll();
+
+  try {
+    db.connect();
+    alert("INFO: BD conectada");
+  } catch (err) {
+    console.error(err);
+    alert(`Error de Base de Datos: ${err.message}`);
+  } finally {
+    Utils.enableAll();
+  }
 }
 
-// function btnConectar() {
-//   Utils.disableAll();
-//   db.connect();
-//   Utils.enableAll();
-// }
+function btnCargar() {
+  Utils.disableAll();
+  const id = prompt("Nuevo ID:");
+  const dato = prompt("Nuevo Dato:");
+  try {
+    db.save(id, dato);
+    alert(`INFO: Nuevo dato cargado (${id}, ${dato})`);
+  } catch (err) {
+    let strTipoError;
 
-// function btnCargar() {
-//   Utils.disableAll();
-//   const id = prompt('Nuevo ID:');
-//   const dato = prompt('Nuevo Dato:');
-//   db.save(id, dato);
-//   Utils.enableAll();
-// }
+    if (err instanceof DBError) {
+      strTipoError = "Base de Datos";
+    } else {
+      strTipoError = "Sistema";
+    }
 
-// function btnDesconectar() {
-//   Utils.disableAll();
-//   db.close();
-//   Utils.enableAll();
-// }
+    console.error(err);
+    alert(`Error de ${strTipoError}: ${err.message}`);
+  } finally {
+    Utils.enableAll();
+  }
+}
+
+function btnLeer() {
+  try {
+    const data = leerDesdeDB();
+    alert(`INFO: Dato recuperado (${data})`);
+    console.info("Resultado:", data);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+}
+
+function btnDesconectar() {
+  Utils.disableAll();
+  try {
+    db.close();
+    alert("INFO: BD desconectada");
+  } catch (err) {
+    console.error(err);
+    alert(`Error de Base de Datos: ${err.message}`);
+  } finally {
+    Utils.enableAll();
+  }
+}
