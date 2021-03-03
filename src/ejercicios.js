6c9d26b9648a488ab14ab3163e19e4d8837bc859
gdsasso <71105600+gdsasso@users.noreply.github.com>
2021-03-03 15:34:34 -0300

    Final

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -1,12 +1,29 @@
 const db = new BaseDatos();
 db.connect();
 
+function btnConectar() {
+  try {
+    Utils.disableAll();
+    db.connect();
+  } catch (err) {
+    alert(`Error al Conectar! \n\n\t ${err.message}`);
+  } finally {
+    Utils.enableAll();
+  }
+}
+
 function leerDesdeDB() {
-  Utils.disableAll();
-  const id = prompt('ID a leer:');
-  const data = db.read(id);
-  Utils.enableAll();
-  return data;
+  try {
+    Utils.disableAll();
+    const id = prompt('ID a leer:');
+    const data = db.read(id);
+    return data;
+  } catch (err) {
+    alert(`Error! \n\n\t ${err.message}`);
+    return null;
+  } finally {
+    Utils.enableAll();  
+  }
 }
 
 function btnLeer() {
@@ -14,22 +31,28 @@ function btnLeer() {
   console.info('Resultado:', data);
 }
 
-// function btnConectar() {
-//   Utils.disableAll();
-//   db.connect();
-//   Utils.enableAll();
-// }
 
-// function btnCargar() {
-//   Utils.disableAll();
-//   const id = prompt('Nuevo ID:');
-//   const dato = prompt('Nuevo Dato:');
-//   db.save(id, dato);
-//   Utils.enableAll();
-// }
 
-// function btnDesconectar() {
-//   Utils.disableAll();
-//   db.close();
-//   Utils.enableAll();
-// }
+function btnCargar() {
+  try {
+    Utils.disableAll();
+    const id = prompt('Nuevo ID:');
+    const dato = prompt('Nuevo Dato:');
+    db.save(id, dato);
+  } catch (err) {
+    alert(`Error al Cargar! \n\n\t ${err.message}`);
+  } finally {
+    Utils.enableAll();
+  }
+}
+
+function btnDesconectar() {
+  try { 
+    Utils.disableAll();
+    db.close();
+  } catch {
+    alert(`Error al Desconectar! \n\n\t ${err.message}`);
+  } finally {
+    Utils.enableAll();
+  }
+ }
