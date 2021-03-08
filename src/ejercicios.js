68530e67c0a138c3f5022ca4bd7f11a85acad0d7
Martin Gonzalez <martin@Martins-MacBook-Pro.local>
2021-03-07 22:15:55 -0300

    Agregado manejo de errores

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -4,11 +4,17 @@ db.connect();
 console.info('Hola');
 
 function leerDesdeDB() {
-  Utils.disableAll();
-  const id = prompt('ID a leer:');
-  const data = db.read(id);
-  Utils.enableAll();
-  return data;
+  try {
+    Utils.disableAll();
+    const id = prompt('ID a leer:');
+    const data = db.read(id);
+    return data;
+  } catch (error) {
+    alert(error);
+    return null
+  } finally {
+    Utils.enableAll();
+  }
 }
 
 function btnLeer() {
@@ -16,22 +22,37 @@ function btnLeer() {
   console.info('Resultado:', data);
 }
 
-// function btnConectar() {
-//   Utils.disableAll();
-//   db.connect();
-//   Utils.enableAll();
-// }
+function btnConectar() {
+  try {
+    Utils.disableAll();
+    db.connect();
+  } catch (error) {
+    alert(error);
+  } finally {
+    Utils.enableAll();
+  }
+}
 
-// function btnCargar() {
-//   Utils.disableAll();
-//   const id = prompt('Nuevo ID:');
-//   const dato = prompt('Nuevo Dato:');
-//   db.save(id, dato);
-//   Utils.enableAll();
-// }
+function btnCargar() {
+  try {
+    Utils.disableAll();
+    const id = prompt('Nuevo ID:');
+    const dato = prompt('Nuevo Dato:');
+    db.save(id, dato);
+  } catch (error) {
+    alert(error);
+  } finally {
+    Utils.enableAll();
+  }
+}
 
-// function btnDesconectar() {
-//   Utils.disableAll();
-//   db.close();
-//   Utils.enableAll();
-// }
+function btnDesconectar() {
+  try {
+    Utils.disableAll();
+    db.close();
+  } catch (error) {
+    alert(error)
+  } finally {
+    Utils.enableAll();
+  }
+}
