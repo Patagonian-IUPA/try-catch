6097bd78000d98795c83f2aa8f1306a884b94f33
Juan <jb@hotmail.com>
2021-03-22 23:19:02 -0300

    Desarrollo y termino TP3

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -1,37 +1,73 @@
 const db = new BaseDatos();
-db.connect();
+
 
 console.info('Hola');
 
 function leerDesdeDB() {
-  Utils.disableAll();
-  const id = prompt('ID a leer:');
-  const data = db.read(id);
-  Utils.enableAll();
-  return data;
+  try {
+    Utils.disableAll();
+    db.connect();
+    const id = prompt('ID a leer:');
+    const data = db.read(id);
+    alert(`ID: ${id} \n\n\t Resultado: ${data}`)
+    return data;
+  } catch (error) {
+    console.error(`Error! \n\n\t ${error}`);
+    alert(`Error! \n\n\t ${error}`)
+  } finally {
+    Utils.enableAll();
+    if (db._connected) {
+      db.close();
+    }
+  }
 }
-
 function btnLeer() {
   const data = leerDesdeDB();
   console.info('Resultado:', data);
 }
 
-// function btnConectar() {
-//   Utils.disableAll();
-//   db.connect();
-//   Utils.enableAll();
-// }
+function btnConectar() {
+  try {
+    Utils.disableAll();
+    db.connect();
+    alert('Conectado a la Base de datos')
+  } catch (error) {
+    console.error(`Error! \n\n\t ${error}`);
+    alert(`Error! \n\n\t ${error}`)
+  } finally {
+    Utils.enableAll();
+  }
+}
 
-// function btnCargar() {
-//   Utils.disableAll();
-//   const id = prompt('Nuevo ID:');
-//   const dato = prompt('Nuevo Dato:');
-//   db.save(id, dato);
-//   Utils.enableAll();
-// }
+function btnCargar() {
+  try {
+    Utils.disableAll();
+    db.connect();
+    const id = prompt('Nuevo ID:');
+    const dato = prompt('Nuevo Dato:');
+    db.save(id, dato);
+    alert (`Dato guardado correctamete \n\t ID: ${id} \n\t Dato: ${dato}`);
+  } catch (error) {
+    console.error(`Error! \n\n\t ${error}`);
+    alert(`Error! \n\n\t ${error}`)
+  } finally {
+    Utils.enableAll();
+    if (db._connected) {
+      db.close();
+    }
+  }
+}
 
-// function btnDesconectar() {
-//   Utils.disableAll();
-//   db.close();
-//   Utils.enableAll();
-// }
+function btnDesconectar() {
+  try {
+    Utils.disableAll();
+    db.close();
+    alert('Desconectado de la Base de datos')
+  } catch (error) {
+    console.error(`Error! \n\n\t ${error}`);
+    alert(`Error! \n\n\t ${error}`)
+  }
+  finally {
+    Utils.enableAll();
+  }
+}
