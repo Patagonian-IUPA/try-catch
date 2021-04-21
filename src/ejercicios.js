2df77e6dad67f3e828cbe4718930970d543204fa
Darío <daentrenamiento@gmail.com>
2021-04-21 09:36:51 -0300

    Modificacion en index.html y ejercicios.js

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -1,37 +1,68 @@
 const db = new BaseDatos();
-db.connect();
+//db.connect();
 
-console.info('Hola');
+console.info('Hola, conectar BD');
 
 function leerDesdeDB() {
+  try{
   Utils.disableAll();
   const id = prompt('ID a leer:');
   const data = db.read(id);
   Utils.enableAll();
   return data;
 }
+  catch(err){
+    alert(`Error!\n\n\t${err.message}`);
+    return null;
+  }
+  finally{
+     Utils.enableAll();
+  }
+}
 
 function btnLeer() {
   const data = leerDesdeDB();
   console.info('Resultado:', data);
 }
+//---------------------------//
 
-// function btnConectar() {
-//   Utils.disableAll();
-//   db.connect();
-//   Utils.enableAll();
-// }
 
-// function btnCargar() {
-//   Utils.disableAll();
-//   const id = prompt('Nuevo ID:');
-//   const dato = prompt('Nuevo Dato:');
-//   db.save(id, dato);
-//   Utils.enableAll();
-// }
+ function btnConectar() {
+  try{
+   Utils.disableAll();
+  
+  db.connect();
+   Utils.enableAll();
+ }
+ catch(error){
+  alert('Error en la conexion a la BD', error);
+ }
+ finally{
 
-// function btnDesconectar() {
-//   Utils.disableAll();
-//   db.close();
-//   Utils.enableAll();
-// }
+   Utils.enableAll();
+    
+}
+ }
+
+ function btnCargar() {
+  try{
+   Utils.disableAll();
+   const id = prompt('Nuevo ID:');
+   const dato = prompt('Nuevo Dato:');
+   db.save(id, dato);
+   Utils.enableAll();
+ }
+ catch(err){
+    alert(`Error al cargar!\n\n\t${err.message}`);
+    
+ }
+ finally{
+    Utils.enableAll();
+ }
+ }
+
+ function btnDesconectar() {
+   Utils.disableAll();
+   db.close();
+   Utils.enableAll();
+ }
